157d7a568032e7933f7a0fe3b8bd06c21b13b58a
Fatih Firdaus <fatihfirdaus@MacBook-Pro-Fatih.local>
2022-02-08 03:02:22 +0700

    phone input

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                     
-                    @if(Auth::user()->roles == 'ADMIN')
+                    @if(Auth::user()->roles == 'USER')
                         <x-jet-nav-link href="{{ route('dashboard.product.index') }}" :active="request()->routeIs('dashboard.product.index')">
                             {{ __('Products') }}
                         </x-jet-nav-link>
@@ -160,7 +160,7 @@
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
 
-            @if(Auth::user()->roles == 'ADMIN')
+            @if(Auth::user()->roles == 'USER')
                 <x-jet-responsive-nav-link href="{{ route('dashboard.product.index') }}" :active="request()->routeIs('dashboard.product.index')">
                     {{ __('Product') }}
                 </x-jet-responsive-nav-link>
